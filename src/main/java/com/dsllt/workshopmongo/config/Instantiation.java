260a5a0814195b7fe7d368f1e5a082a041365b04
dsllt <dayanesallet@hotmail.com>
2024-03-10 09:47:18 -0300

    Getting user data from userDTO

--- src/main/java/com/dsllt/workshopmongo/config/Instantiation.java
+++ src/main/java/com/dsllt/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.dsllt.workshopmongo.domain.Post;
 import com.dsllt.workshopmongo.domain.User;
+import com.dsllt.workshopmongo.dto.AuthorDTO;
 import com.dsllt.workshopmongo.repository.PostRepository;
 import com.dsllt.workshopmongo.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
     User alex = new User(null, "Alex Green", "alex@gmail.com"); 
     User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-    Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", maria);
-    Post post2 = new Post(null, sdf.parse("22/03/2024"), "Bom dia", "Acordei feliz!", maria);
-
     userRepository.saveAll(Arrays.asList(maria, alex, bob));
+    
+    Post post1 = new Post(null, sdf.parse("21/03/2024"), "Partiu viagem", "Vou viajar para São Paulo, abraços!", new AuthorDTO(maria));
+    Post post2 = new Post(null, sdf.parse("22/03/2024"), "Bom dia", "Acordei feliz!", new AuthorDTO(maria));
+
     postRepository.saveAll(Arrays.asList(post1, post2));
   }
   
