26429418e6ed7753aad8100f67e8e9ebc12ce76f
dsllt <dayanesallet@hotmail.com>
2024-03-10 08:38:28 -0300

    Get user by id and exception handling

--- src/main/java/com/dsllt/workshopmongo/services/UserService.java
+++ src/main/java/com/dsllt/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.dsllt.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dsllt.workshopmongo.domain.User;
 import com.dsllt.workshopmongo.repository.UserRepository;
+import com.dsllt.workshopmongo.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 
   public List<User> findAll(){
     return repo.findAll();
+  }
+
+  public User findById(String id){
+    Optional<User> obj = repo.findById(id);
+    return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found."));
   }
 }
